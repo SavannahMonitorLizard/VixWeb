e04b73b343577de767bbf336632056a2e474c6b6
TymurTkachenko <tymur.tk@gmail.com>
2020-03-28 17:59:18 -0400

    cboe web scraper

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from requests_html import HTMLSession
 import matplotlib.pyplot as plt
 
 session = HTMLSession()
-r = session.get("http://www.vixcentral.com/")
+r = session.get("http://www.cboe.com/products/futures/vx-cboe-volatility-index-vix-futures")
 
 dates, values = [], []
 
@@ -16,44 +16,71 @@ while True:
     except pyppeteer.errors.TimeoutError:
         print("Could not load page, retrying in 30 seconds")
         time.sleep(30)
-        continue
 
-    data = r.html.find("#basicTable", first=True)
+    data = r.html.find("#itemPlaceholderContainer1", first=True)
     dataSplit = data.text.split("\n")
-    contango = dataSplit[:16]
-    difference = dataSplit[16:]
-    contango.insert(0, datetime.datetime.now())
-    difference.insert(0, datetime.datetime.now())
+    dataCut = []
 
-    try:    
-        csv_file = open("vixData.csv", "a", newline="")
-    except PermissionError:
-        print("Could not load file due to file being open, retrying in 5 minutes")
-        time.sleep(300)
-        continue
+    for i in dataSplit[8:]:
+        dataCut.append(i)
 
-    writer = csv.writer(csv_file)
-    writer.writerow(contango)
-    writer.writerow(difference)
-    writer.writerow([])
-    csv_file.close()
-    print("Writing succesful")
+    chunks = [dataCut[x:x+8] for x in range(0, len(dataCut), 8)]
 
-    with open("values_dates.json") as load_file:
-        values_dates = json.load(load_file)
-    values = values_dates["values"]
-    dates = values_dates["dates"]
-    values.append(contango[3])
-    dates.append(datetime.datetime.now().strftime("%a, %b %d, %Y, %H:%M:%S"))
-    values_dates = {}
-    values_dates.setdefault("values", values)
-    values_dates.setdefault("dates", dates)
-    print(values_dates)
-    with open("values_dates.json", "w") as dump_file:
-        json.dump(values_dates, dump_file)
-    
-    plt.plot(dates, values, "-o")
-    plt.savefig("vixGraph.png")
+    validLists = []
+
+    for i in chunks:
+        if not i[0][2].isdigit():
+            validLists.append(i)
+
+    for i in validLists:
+        currentList = " ".join(i)
+        print(currentList)
     
     time.sleep(300)
     print("Beginning again")
+
+# try:
+#     r.html.render(timeout=30)
+#     print("Page successfully loaded")
+# except pyppeteer.errors.TimeoutError:
+#     print("Could not load page, retrying in 30 seconds")
+#     time.sleep(30)
+
+# data = r.html.find("#itemPlaceholderContainer1", first=True)
+# dataSplit = data.text.split("\n")
+# dataCut = []
+
+# for i in dataSplit[8:]:
+#     dataCut.append(i)
+
+# chunks = [dataCut[x:x+8] for x in range(0, len(dataCut), 8)]
+
+# validLists = []
+
+# for i in chunks:
+#     if not i[0][2].isdigit():
+#         validLists.append(i)
+
+# for i in validLists:
+#     currentList = " ".join(i)
+#     print(currentList)
+# contango = dataSplit[:16]
+# difference = dataSplit[16:]
+# contango.insert(0, datetime.datetime.now())
+# difference.insert(0, datetime.datetime.now())
+
+# try:    
+#     csv_file = open("vixData.csv", "a", newline="")
+# except PermissionError:
+#     print("Could not load file due to file being open, retrying in 5 minutes")
+#     time.sleep(300)
+
+# writer = csv.writer(csv_file)
+# writer.writerow(contango)
+# writer.writerow(difference)
+# writer.writerow([])
+# csv_file.close()
+# print("Writing succesful")
+
+# time.sleep(300)
+# print("Beginning again")
